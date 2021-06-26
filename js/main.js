5a65591c449bd61047d811fd50e0b29949c3ab44
Libia Perez <alexandra220790@gmail.com>
2021-06-26 15:56:44 -0300

    estilizando menu para mobile y desktop

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 let buttonMenu = document.getElementById("buttonMenu");
+let overlay = document.getElementById("overlay");
 buttonMenu.addEventListener("click", showMenu);
 let menu = document.querySelector(".menu");
 
 function showMenu() {
   menu.classList.toggle("hidden");
+  overlay.classList.toggle("overlay");
 }
